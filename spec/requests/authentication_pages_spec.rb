94bf1536df70992c1c9a4612f26124436d8a77ac
ServerJunge <serverjunge@live.de>
2014-07-27 18:31:59 +0200

    Cmoiot

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -9,9 +9,9 @@ describe "Authentication" do
 
     it { should have_content('Sign in') }
     it { should have_title('Sign in') }
+  end
 
-    describe "signin" do
-
+  describe "signin" do
     before { visit signin_path }
 
     describe "with invalid information" do
@@ -19,11 +19,6 @@ describe "Authentication" do
 
       it { should have_title('Sign in') }
       it { should have_selector('div.alert.alert-error') }
-
-      describe "after visiting another page" do
-        before { click_link "Home" }
-        it { should_not have_selector('div.alert.alert-error') }
-      end
     end
 
     describe "with valid information" do
@@ -34,13 +29,10 @@ describe "Authentication" do
         click_button "Sign in"
       end
 
-
       it { should have_title(user.name) }
       it { should have_link('Profile',     href: user_path(user)) }
       it { should have_link('Sign out',    href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
-    end
-
     end
   end
 end
